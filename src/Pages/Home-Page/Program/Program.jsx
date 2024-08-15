f99d0704c89a4ba44d156ed0c6b13841de6ab235
Panchammmm <sardarpancham510510@gmail.com>
2024-08-15 14:05:06 +0530

    programs-section-responsive

--- src/Pages/Home-Page/Program/Program.jsx
+++ src/Pages/Home-Page/Program/Program.jsx
@@ -13,20 +13,19 @@ const Program = () => {
 
     return (
         <div className="bg-gray-100 py-[120px]">
-            <h2 className="text-4xl font-bold text-center mb-16">
+            <h2 className="text-4xl font-bold text-center mb-10 lg:mb-20">
                 Our <span className="text-orange-500">Programs</span>
             </h2>
 
-            <div className="flex flex-wrap px-20 align-middle justify-center">
-
+            <div className="flex flex-wrap items-center justify-center px-4 lg:px-20">
                 {/* Image Section */}
-                <div className="scale-[1.5] pr-10 -translate-y-10">
-                    <img src={pro} alt="Students" className="h-[45vh]" />
+                <div className="w-full lg:w-auto lg:scale-[1.5] lg:pr-10 lg:-translate-y-10 mb-16 lg:mb-0">
+                    <img src={pro} alt="Students" className="h-[45vh] mx-auto lg:mx-0" />
                 </div>
 
                 {/* Text Section */}
-                <div className="w-1/2 pl-12 flex justify-start">
-                    <div className="grid grid-cols-2 gap-6">
+                <div className="w-full lg:w-1/2 lg:pl-12 flex justify-center lg:justify-start">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
                         {features.map((feature, index) => (
                             <div key={index} className="flex items-start">
                                 <feature.icon size={35} className="text-orange-500 text-3xl mr-4" />
